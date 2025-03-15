cbe481f37c40da3a1b011245d89b56b5d0b066df
Renlane <67070079@kmitl.ac.th>
2025-03-15 20:47:45 +0700

    file non block with bug

--- learn/fs/fs_FileSystem.js
+++ learn/fs/fs_FileSystem.js
@@ -5,8 +5,21 @@
 //Blocking sync
 const fs = require('fs') //module มีอยู่แล้ว
 
-const data = fs.readFileSync('fs/file/input.txt','utf-8')
-console.log(data)
-console.log('จบการทำงาน')
+// const data = fs.readFileSync('../fs/file/input.txt','utf-8')
+// console.log(data)
+// console.log('จบการทำงาน')
 
-const outputText = `Hello Node.js\n${data} ถูกเขียนเมื่อ ${new Date()}`
+// const outputText = `Hello Node.js\n${data}\nถูกเขียนเมื่อ ${new Date()}`
+// fs.writeFileSync("../fs/file/output.txt",outputText)
+// console.log("done")
+
+//Non-Blocking async
+fs.readFile('fs/file/input.txt','utf-8',(err,data)=>{
+    if(err) return console.log("err",err)
+    console.log(data);
+    const outputText = `Hello Node.js\n${data}\nถูกเขียนเมื่อ ${new Date()}`
+    fs.writeFile('fs/file/output.txt',outputText,err=>{
+        if(err) return console.log("err",err)
+        console.log("จบการทำงาน")
+    })
+})
